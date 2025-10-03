70ffc0777151c9576a0b70507ff15aeb3566ab9c
Aroh Abuchi <arohabuchi@gmail.com>
2025-10-03 07:16:40 +0100

    first commit

--- src/components/landingComponoent/footer.tsx
+++ src/components/landingComponoent/footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
         <div>
             <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Kudisphere</h1>
-            <p className='py-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, minima!</p>
+            <p className='py-4'>Borderless transaction on the Go!</p>
             <div className='flex justify-between md:w-[75%] my-6'>
                 <FaFacebook size={30}/>
                 <FaInstagram size={30} />
