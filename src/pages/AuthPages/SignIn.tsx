957d34ced83acb9213112e098ddc9589b546abbb
Aroh Abuchi <arohabuchi@gmail.com>
2025-10-08 21:27:53 +0100

    first commit

--- src/pages/AuthPages/SignIn.tsx
+++ src/pages/AuthPages/SignIn.tsx
@@ -54,8 +54,8 @@ export default function SignIn() {
   return (
     <>
       <PageMeta
-        title="React.js SignIn Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js SignIn Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="KUDISPHERE! Send & Receive crypto funds"
+        description="Welcome to kudisphere! go borderless in your financial transaction"
       />
       <AuthLayout>
         <SignInForm  />
