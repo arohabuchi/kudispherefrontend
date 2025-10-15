4bc80733c4bd05818ef69454361bbbcce34c25e0
Aroh Abuchi <arohabuchi@gmail.com>
2025-10-15 21:42:13 +0100

    first commit

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -176,9 +176,7 @@ interface ModalProps {
 
   return (
     <div className="absolute top-0 left-0 right-0 z-50 flex justify-center bg-black bg-opacity-60 backdrop-blur-sm p-4 md:inset-0 md:items-center md:justify-center">
-      <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-xl w-full mx-auto 
-max-w-xs sm:max-w-lg md:max-w-2xl 
-p-4 sm:p-6 lg:p-10 relative">
+      <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-xl w-full mx-auto max-w-xs sm:max-w-lg md:max-w-2xl p-4 sm:p-6 lg:p-10 ">
         {/* Close button */}
         <button
           onClick={onClose}
